d5a47b3e1b44071041586c68ed91cf8edd3717ba
asmsuechan <suenagaryoutaabc@gmail.com>
2020-06-19 17:29:40 +0900

    Bump to v0.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.jvm.tasks.Jar
 import com.jfrog.bintray.gradle.BintrayExtension
 
 group = "com.rowma.rowma-kotlin"
-version = "0.0.3"
+version = "0.0.4"
 val artifactID = "rowma-kotlin"
 val publicationName = "default"
 
