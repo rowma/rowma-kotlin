4e76ba3ca90ac3b8321cae781786708870bf5ab9
asmsuechan <suenagaryoutaabc@gmail.com>
2021-01-20 15:54:48 +0900

    Bump to v1.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.jvm.tasks.Jar
 import com.jfrog.bintray.gradle.BintrayExtension
 
 group = "com.rowma.rowma-kotlin"
-version = "1.1.0"
+version = "1.1.1"
 val artifactID = "rowma-kotlin"
 val publicationName = "default"
 
